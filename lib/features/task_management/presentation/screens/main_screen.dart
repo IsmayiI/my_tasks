f23c3147ed35e8a980bb236b39bb7645293756f9
Ismayil <ismayilov_ismayil_97@mail.ru>
2025-12-21 19:17:26 +0400

    add bottom navbar

--- lib/features/task_management/presentation/screens/main_screen.dart
+++ lib/features/task_management/presentation/screens/main_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:my_tasks/features/authentication/presentation/controllers/auth_controller.dart';
+import 'package:my_tasks/features/authentication/presentation/screens/accounts_screen.dart';
+
+import 'screens.dart';
 
 class MainScreen extends ConsumerStatefulWidget {
   const MainScreen({super.key});
@@ -9,21 +11,72 @@ class MainScreen extends ConsumerStatefulWidget {
   ConsumerState<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends ConsumerState<MainScreen> {
+class _MainScreenState extends ConsumerState<MainScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  int currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 5, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
+    _tabController.index = currentIndex;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Main Screen'),
-        actions: [
-          ElevatedButton(
-            onPressed: () {
-              ref.read(authControllerProvider.notifier).signOut();
-            },
-            child: Text('out'),
+      body: TabBarView(
+        controller: _tabController,
+        children: const [
+          AllTaskScreen(),
+          InCompleteTasksScreen(),
+          AddTaskScreen(),
+          CompletedTasksScreen(),
+          AccountsScreen(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (value) => setState(() => currentIndex = value),
+        iconSize: 20,
+        elevation: 5,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home_outlined),
+            activeIcon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: 'Incomplete',
+            icon: Icon(Icons.dangerous_outlined),
+            activeIcon: Icon(Icons.dangerous),
+          ),
+          BottomNavigationBarItem(
+            label: 'Add',
+            icon: Icon(Icons.add),
+            activeIcon: Icon(Icons.add),
+          ),
+          BottomNavigationBarItem(
+            label: 'Complete',
+            icon: Icon(Icons.check_box_outlined),
+            activeIcon: Icon(Icons.check_box),
+          ),
+          BottomNavigationBarItem(
+            label: 'Account',
+            icon: Icon(Icons.person_outlined),
+            activeIcon: Icon(Icons.person),
           ),
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 }
---
+++ lib/features/task_management/presentation/screens/screens.dart
@@ -0,0 +1,5 @@
+export 'add_task_screen.dart';
+export 'all_task_screen.dart';
+export 'completed_tasks_screen.dart';
+export 'in_complete_tasks_screen.dart';
+export 'main_screen.dart';
