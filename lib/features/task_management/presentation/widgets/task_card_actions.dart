2c62f3505f510a1d5fb278b4574ab47e1dd66748
Ismayil <ismayilov_ismayil_97@mail.ru>
2025-12-26 15:07:32 +0400

    add edit logic to task card

--- lib/features/task_management/presentation/widgets/task_card_actions.dart
+++ lib/features/task_management/presentation/widgets/task_card_actions.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:my_tasks/features/authentication/data/auth_repository.dart';
 import 'package:my_tasks/features/task_management/domain/task.dart';
 import 'package:my_tasks/features/task_management/presentation/controllers/store_controller.dart';
 import 'package:my_tasks/utils/utils.dart';
 
-class TaskCardActions extends ConsumerWidget {
+class TaskCardActions extends ConsumerStatefulWidget {
   final Task task;
   const TaskCardActions({super.key, required this.task});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<TaskCardActions> createState() => _TaskCardActionsState();
+}
+
+class _TaskCardActionsState extends ConsumerState<TaskCardActions> {
+  @override
+  Widget build(BuildContext context) {
     final userId = ref.watch(currentUserProvider)!.uid;
 
     return Column(
@@ -21,14 +27,14 @@ class TaskCardActions extends ConsumerWidget {
           scale: 1.8,
           child: Checkbox(
             visualDensity: VisualDensity.compact,
-            value: task.isComplete,
+            value: widget.task.isComplete,
             onChanged: (value) {
               if (value != null) {
                 ref
                     .read(storeControllerProvider.notifier)
                     .updateTaskCompletion(
                       isComplete: value,
-                      taskId: task.id,
+                      taskId: widget.task.id,
                       userId: userId,
                     );
               }
@@ -48,7 +54,7 @@ class TaskCardActions extends ConsumerWidget {
             padding: EdgeInsets.zero,
             tapTargetSize: MaterialTapTargetSize.shrinkWrap,
           ),
-          onPressed: () => _editTask(ref, userId),
+          onPressed: () => _editTask(userId),
           icon: const Icon(Icons.edit),
         ),
 
@@ -64,22 +70,80 @@ class TaskCardActions extends ConsumerWidget {
             padding: EdgeInsets.zero,
             tapTargetSize: MaterialTapTargetSize.shrinkWrap,
           ),
-          onPressed: () => _deleteTask(ref, userId),
+          onPressed: () => _deleteTask(userId),
           icon: const Icon(Icons.delete),
         ),
       ],
     );
   }
 
-  void _deleteTask(WidgetRef ref, String userId) {
+  void _deleteTask(String userId) {
     ref
         .read(storeControllerProvider.notifier)
-        .deleteTask(taskId: task.id, userId: userId);
+        .deleteTask(taskId: widget.task.id, userId: userId);
   }
 
-  void _editTask(WidgetRef ref, String userId) {
-    ref
-        .read(storeControllerProvider.notifier)
-        .updateTask(task: task, userId: userId);
+  void _editTask(String userId) {
+    TextEditingController titleController = TextEditingController(
+      text: widget.task.title,
+    );
+    TextEditingController descriptionController = TextEditingController(
+      text: widget.task.description,
+    );
+
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        icon: const Icon(Icons.edit, color: Colors.green, size: 40),
+        title: Text('Update Task', style: AppTextStyle.normal),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: const InputDecoration(labelText: 'Title'),
+            ),
+            TextField(
+              controller: descriptionController,
+              decoration: const InputDecoration(labelText: 'Description'),
+            ),
+          ],
+        ),
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  context.pop();
+                },
+                child: Text('Cancel', style: AppTextStyle.normal),
+              ),
+              AppSpacer.vertical(20),
+              ElevatedButton(
+                onPressed: () async {
+                  final task = Task(
+                    id: widget.task.id,
+                    title: titleController.text.trim(),
+                    description: descriptionController.text.trim(),
+                    isComplete: widget.task.isComplete,
+                    priority: widget.task.priority,
+                    date: widget.task.date,
+                  );
+                  await ref
+                      .read(storeControllerProvider.notifier)
+                      .updateTask(task: task, userId: userId);
+                  if (mounted) {
+                    context.pop();
+                  }
+                },
+                child: Text('Update', style: AppTextStyle.normal),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
